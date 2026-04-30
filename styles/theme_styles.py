33bd55af600de52d2c15c1161ae9455c9e8cf573
eduar <eduar@EDUARDO>
2026-04-30 17:58:15 -0400

    update

--- styles/theme_styles.py
+++ styles/theme_styles.py
@@ -1,7 +1,8 @@
 THEME_STYLES = """
 
 :root{
-   --global_background: #fcfcfc;
+   /* --global_background: #fcfcfc; */
+   --global_background: #e8e8e8;
    /*--global_foreground: #343434;*/
    --global_foreground: #555;
    /*--accent_color_01: #ee5700;*/
