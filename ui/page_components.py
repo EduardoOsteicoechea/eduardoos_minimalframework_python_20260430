187e796d8ff182b6971be6ac8b8b41b0946ddc9e
eduar <eduar@EDUARDO>
2026-05-01 06:41:48 -0400

    update

--- ui/page_components.py
+++ ui/page_components.py
@@ -6,7 +6,7 @@ def page_top_start(title="App") -> str:
     js_static_assets_route = "/static/js"
     return f"""
 <!DOCTYPE html>
-<html lang="en"">
+<html lang="en">
    <head>
       <meta charset="UTF-8">      
       <meta name="viewport" content="width=device-width, initial-scale=1" />
